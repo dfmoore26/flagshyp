9f8ae2ac51f61ab3e660f09deab499511ff5c5ea
dfmoore26 <46910129+dfmoore26@users.noreply.github.com>
2019-04-25 00:00:06 -0400

    ctens26.m: Mooney-Rivlin

--- code/constitutive laws/elasticity_tensor/ctens26.m
+++ code/constitutive laws/elasticity_tensor/ctens26.m
@@ -15,8 +15,20 @@ I2_bar = (J^(-4/3))*I2;
 C_bar = (J^(-2/3))*C;
 C_bar_inv = inv(C_bar);
 I = eye(length(J));
+I_double = eye(dim,dim,dim,dim);
+c = zeros(dim,dim,dim,dim);
+c_vol = zeros(dim,dim,dim,dim);
+c_iso = zeros(dim,dim,dim,dim);
 
-S_iso = (J^(-2/3))*((-1/3)*(mu_1*I1_bar + 2*mu_2*I2_bar)*C_bar_inv + (mu_1 + mu_2*I1_bar)*I - mu_2*C);
-S_vol = (J^(1/3))*(J-1)*k*C_bar_inv;
-
-S_Kirchoff = S_iso + S_vol;
+for l = 1:dim
+  for k = 1:dim
+    for j = 1:dim
+      for i = 1:dim
+        c_vol(i,j,k,l) = (J^(-1/3))*(2*J - 1)*K*(C_bar_inv(i,j)*C_bar_inv(k,l)) - 2*(J^(-1/3))*(J-1)*K*(C_bar_inv(i,l)*C_bar_inv(j,k));
+        c_iso(i,j,k,l) = 2*(J^(-4/3))*mu_2*(I(i,j)*I(k,l) - I_double) - (2/3)*(J^(-4/3))*(mu_1 + 2*mu_2*I1_bar)*(C_bar_inv(i,j)*I(k,l) + I(i,j)*C_bar_inv(k,l)) + (4/3)*(J^(-4/3))*mu_2*(C_bar_inv(i,j)*C_bar(k,l) + C_bar(i,j)*C_bar_inv(k,l)) + (2/9)*(J^(-4/3))*(mu_1*I1_bar = 4*mu_2*I2_bar)*(C_inv_bar(i,j)*C_inv_bar(k,l)) + (2/3)*(J^(-4/3))*(mu_1*I1_bar + 2*mu_2*I2_bar)*(C_inv_bar(i,l)*C_inv_bar(j,k));
+        
+        c(i,j,k,l) = c_vol(i,j,k,l) + c_iso(i,j,k,l);
+      end
+    end
+  end
+end
